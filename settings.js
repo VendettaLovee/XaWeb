580701c6a20fa3720d477f916abdf5722b60d2c3
VendettaLovee <lisiana000@gmail.com>
2025-04-30 23:30:21 +0700

    Upload website public By Vendetta

---
+++ settings.js
@@ -0,0 +1,14 @@
+export const API_KEY = ['free'];
+export const CREATOR = '@Xavier Api'
+
+export const mess = {
+  inapikey: "invalid apiKey",
+  apikey: "apiKey required",
+  method: "method not allowed",
+  error: "internal server error"
+};
+
+export const footer = {
+  name: "Vendetta",
+  url: "https://github.com/VendettaLovee"
+};
